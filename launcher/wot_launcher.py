4437a2e43cb3f1db6ebfa03f71ea1634af5b3b4e
Peng Wang <peng.wang@lumalabs.ai>
2026-09-06 03:02:31 -0400

    Revert "Release version 0.6.11"
    This reverts commit 99b781296464704c3dd5c4753e389a248282cb26.

--- launcher/wot_launcher.py
+++ launcher/wot_launcher.py
@@ -28,7 +28,7 @@ else:
         vehicle_editor_ui, vehicle_overlays)
 
 
-LAUNCHER_VERSION = "0.6.11"
+LAUNCHER_VERSION = "0.6.10"
 WINDOW_TITLE = "World of Tanks Offline Battles %s" % LAUNCHER_VERSION
 
 _CHINESE = {
